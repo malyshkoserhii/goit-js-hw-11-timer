64a4653bf4b30ffee4b65563809960cc8b71124b
Malyshko Serhii <malyshkosergiy@gmail.com>
2020-09-22 10:54:47 +0300

    task-03 has been finished

--- js/task-03.js
+++ js/task-03.js
@@ -1,16 +1,44 @@
-function updateClockface(time) {
-  /**
-   * Копипаста со стека
-   */
+const daysRef = document.querySelector('span[data-value="days"]');
+const hoursRef = document.querySelector('span[data-value="hours"]');
+const minutesRef = document.querySelector('span[data-value="mins"]');
+const secondsRef = document.querySelector('span[data-value="secs"]');
+
+const timer = {
+  targetDate: new Date("Jan 01, 2021"),
+
+  newYearTimer() {
+    const targetDateInMs = this.targetDate.getTime();
+
+    updateTimer(0);
+
+    setInterval(() => {
+      const currentDate = Date.now();
+      const time = targetDateInMs - currentDate;
+      updateTimer(time);
+    }, 1000);
+  },
+};
+
+timer.newYearTimer();
+
+function updateTimer(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
   const hours = pad(
     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
   );
   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
-  refs.clockface.textContent = `${hours}:${mins}:${secs}`;
+  daysRef.textContent = `${days}`;
+  hoursRef.textContent = `${hours}`;
+  minutesRef.textContent = `${mins}`;
+  secondsRef.textContent = `${secs}`;
 }
 
 function pad(value) {
+  if (value >= 100) {
+    return String(value).padStart(3, "0");
+  }
   return String(value).padStart(2, "0");
 }
