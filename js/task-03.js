6dee21edff3162c04f94a830ce824825c9dd6066
Malyshko Serhii <malyshkosergiy@gmail.com>
2020-10-05 13:36:44 +0300

    class CountdownTimer has been corrected

--- js/task-03.js
+++ js/task-03.js
@@ -1,41 +1,44 @@
-const daysRef = document.querySelector('span[data-value="days"]');
-const hoursRef = document.querySelector('span[data-value="hours"]');
-const minutesRef = document.querySelector('span[data-value="mins"]');
-const secondsRef = document.querySelector('span[data-value="secs"]');
-
 class CountdownTimer {
   constructor(targetDate) {
     this.targetDate = new Date(targetDate);
+    this.daysRef = document.querySelector('span[data-value="days"]');
+    this.hoursRef = document.querySelector('span[data-value="hours"]');
+    this.minutesRef = document.querySelector('span[data-value="mins"]');
+    this.secondsRef = document.querySelector('span[data-value="secs"]');
   }
 
   countdown() {
     const targetDateInMs = this.targetDate.getTime();
 
-    this.updateTimer(0);
+    this._updateTimer(0);
 
     setInterval(() => {
       const currentDate = Date.now();
       const time = targetDateInMs - currentDate;
-      this.updateTimer(time);
+      this._updateTimer(time);
     }, 1000);
   }
 
-  updateTimer(time) {
-    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  _updateTimer(time) {
+    const days = this._pad(Math.floor(time / (1000 * 60 * 60 * 24)));
 
-    const hours = this.pad(
+    const hours = this._pad(
       Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     );
-    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    const mins = this._pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this._pad(Math.floor((time % (1000 * 60)) / 1000));
 
-    daysRef.textContent = `${days}`;
-    hoursRef.textContent = `${hours}`;
-    minutesRef.textContent = `${mins}`;
-    secondsRef.textContent = `${secs}`;
+    this._markup(days, hours, mins, secs);
   }
 
-  pad(value) {
+  _markup(days, hours, mins, secs) {
+    this.daysRef.textContent = `${days}`;
+    this.hoursRef.textContent = `${hours}`;
+    this.minutesRef.textContent = `${mins}`;
+    this.secondsRef.textContent = `${secs}`;
+  }
+
+  _pad(value) {
     if (value >= 100) {
       return String(value).padStart(3, "0");
     }
